e4e5ed097d2453b7a28c28fe773ca83d42dc66d8
CristianGabriel <desktop@binaryfusion.ro>
2024-05-17 12:57:28 +0300

    Am adaugat buton de testat API Gateway

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState('Press the button');
+  const [error, setError] = useState(null);
+
+  const fetchData = async () =>  {
+    const body = {
+      firstName: 'cristi',
+      lastName: 'dani',
+    };
+
+    try {
+      const response = await fetch('https://v68vdaivab.execute-api.eu-central-1.amazonaws.com/on_path_robotics_2', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error: Status ${response.status}`);
+      }
+      let postsData = await response.json();
+      setData(postsData);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    } finally {
+
+    }
+  };
+
+  const LambdaButton = () => {
+    const testLambdaFc = () => {
+      fetchData().then(response => setData(response));
+    };
+
+    return <div className='btn' role="button" tabIndex={0} onClick={testLambdaFc}>Test Lambda function</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            On Path Robotics 2
+        <p> On Path Robotics 2
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <LambdaButton></LambdaButton>
+      <p>{data}</p>
     </div>
   );
 }
